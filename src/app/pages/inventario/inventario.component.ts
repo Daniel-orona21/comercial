9e60109c063a9f9e8b7aac10e313a4e55c000342
Alejandor Daniel López Orona <128829840+Daniel-orona21@users.noreply.github.com>
2025-06-12 08:24:27 -0600

    inventario-general

--- src/app/pages/inventario/inventario.component.ts
+++ src/app/pages/inventario/inventario.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-inventario',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './inventario.component.html',
-  styleUrl: './inventario.component.css'
+  styleUrls: ['./inventario.component.scss'],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class InventarioComponent {
 
