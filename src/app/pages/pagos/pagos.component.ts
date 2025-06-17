0bcd6df9885c3e17889e5ad959c832d933978e7a
Alejandor Daniel López Orona <128829840+Daniel-orona21@users.noreply.github.com>
2025-06-17 10:04:11 -0600

    pagos

--- src/app/pages/pagos/pagos.component.ts
+++ src/app/pages/pagos/pagos.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-pagos',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './pagos.component.html',
-  styleUrl: './pagos.component.css'
+  styleUrls: ['./pagos.component.scss'],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class PagosComponent {
 
