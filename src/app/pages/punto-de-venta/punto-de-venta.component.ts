9f11a3024c9bc517000ca7896b4a43d1c2b84562
Alejandor Daniel López Orona <128829840+Daniel-orona21@users.noreply.github.com>
2025-06-20 07:56:55 -0600

    ventas

--- src/app/pages/punto-de-venta/punto-de-venta.component.ts
+++ src/app/pages/punto-de-venta/punto-de-venta.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-punto-de-venta',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './punto-de-venta.component.html',
   styleUrl: './punto-de-venta.component.scss'
 })
 export class PuntoDeVentaComponent {
+  activeTab: string = 'ventas';
 
+  selectTab(tabName: string) {
+    this.activeTab = tabName;
+  }
 }
